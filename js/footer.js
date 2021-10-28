20ad657ef12b642a2b12f64dfeae0bfdf1f2b563
AshikaChadaga <ashikachadaga@gmail.com>
2021-10-28 22:22:04 +0530

    ADDED: Dynamic UI design for Footer

---
+++ js/footer.js
@@ -0,0 +1,38 @@
+document.getElementById("footer-section").innerHTML =
+    `<footer id="footer">
+    <div class="footer-section">
+        <img class="footer-logo" src="../assets/gallery/logo-wheel.png" alt="">
+        <p class="footer-tag">© Discovery Land Company. All rights reserved.</p>
+    </div>
+    <div class="footer-section">
+        <ul>
+            <li style="font-size: 15px;">GET IN TOUCH</li>
+            <li>(480) 624-5200</li>
+            <li><a href="mailto:mail@discoverylandco.com">mail@discoverylandco.com</a></li>
+        </ul>
+    </div>
+    <div class="footer-section">
+        <ul>
+            <li><a href="../pages/about.html">About DLC</a></li>
+            <li><a href="../pages/our_worlds.html">Our Worlds</a></li>
+            <li><a href="../pages/experience.html">Experience</a></li>
+            <li><a href="../pages/gallery.html">Gallery</a></li>
+            <li><a href="../pages/press.html">Press</a></li>
+        </ul>
+    </div>
+    <div class="footer-section">
+        <ul>
+            <li><a href="../pages/magazine.html">Magazine</a></li>
+            <li><a href="../pages/careers.html">Careers</a></li>
+            <li><a href="../pages/foundation.html">Foundation</a></li>
+            <li><a href="../pages/company.html">Company</a></li>
+            <li><a href="../pages/contact.html">Contact</a></li>
+            <li><a href="../pages/privacy.html">Privacy</a></li>
+        </ul>
+    </div>
+</footer>`;
+
+
+                
+                
+                

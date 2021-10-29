b4554c75b5174a792d7c2defa42228e00ca77674
AshikaChadaga <ashikachadaga@gmail.com>
2021-10-29 12:13:13 +0530

    MODIFIED: Class attribute created

--- js/footer.js
+++ js/footer.js
@@ -1,38 +1,33 @@
 document.getElementById("footer-section").innerHTML =
-    `<footer id="footer">
+    `<footer class="footer" id="footer">
     <div class="footer-section">
         <img class="footer-logo" src="../assets/gallery/logo-wheel.png" alt="">
         <p class="footer-tag">© Discovery Land Company. All rights reserved.</p>
     </div>
     <div class="footer-section">
-        <ul>
-            <li style="font-size: 15px;">GET IN TOUCH</li>
-            <li>(480) 624-5200</li>
-            <li><a href="mailto:mail@discoverylandco.com">mail@discoverylandco.com</a></li>
+        <ul class="footer-list">
+            <li class="footer-links" style="font-size: 15px;">GET IN TOUCH</li>
+            <li class="footer-links">(480) 624-5200</li>
+            <li class="footer-links"><a class="menu" href="mailto:mail@discoverylandco.com">mail@discoverylandco.com</a></li>
         </ul>
     </div>
     <div class="footer-section">
-        <ul>
-            <li><a href="../pages/about.html">About DLC</a></li>
-            <li><a href="../pages/our_worlds.html">Our Worlds</a></li>
-            <li><a href="../pages/experience.html">Experience</a></li>
-            <li><a href="../pages/gallery.html">Gallery</a></li>
-            <li><a href="../pages/press.html">Press</a></li>
+        <ul class="footer-list">
+            <li class="footer-links"><a class="menu" href="../pages/about.html">About DLC</a></li>
+            <li class="footer-links"><a class="menu" href="../pages/our_worlds.html">Our Worlds</a></li>
+            <li class="footer-links"><a class="menu" href="../pages/experience.html">Experience</a></li>
+            <li class="footer-links"><a class="menu" href="../pages/gallery.html">Gallery</a></li>
+            <li class="footer-links"><a class="menu" href="../pages/press.html">Press</a></li>
         </ul>
     </div>
     <div class="footer-section">
-        <ul>
-            <li><a href="../pages/magazine.html">Magazine</a></li>
-            <li><a href="../pages/careers.html">Careers</a></li>
-            <li><a href="../pages/foundation.html">Foundation</a></li>
-            <li><a href="../pages/company.html">Company</a></li>
-            <li><a href="../pages/contact.html">Contact</a></li>
-            <li><a href="../pages/privacy.html">Privacy</a></li>
+        <ul class="footer-list">
+            <li class="footer-links"><a class="menu" href="../pages/magazine.html">Magazine</a></li>
+            <li class="footer-links"><a class="menu" href="../pages/careers.html">Careers</a></li>
+            <li class="footer-links"><a class="menu" href="../pages/foundation.html">Foundation</a></li>
+            <li class="footer-links"><a class="menu" href="../pages/company.html">Company</a></li>
+            <li class="footer-links"><a class="menu" href="../pages/contact.html">Contact</a></li>
+            <li class="footer-links"><a class="menu" href="../pages/privacy.html">Privacy</a></li>
         </ul>
     </div>
 </footer>`;
-
-
-                
-                
-                
